d2e6213c0adabe3757882d110f2a87f1822d6995
isaac99leal <isaac99leal@gmail.com>
2026-09-05 09:37:34 -0400

    Make legal-aware vineyard engine the public default

--- sommelier_v2/knowledge/__init__.py
+++ sommelier_v2/knowledge/__init__.py
@@ -7,25 +7,31 @@ from .fermentation_engine import AlcoholicFermentationParams, FermentationState,
 from .fermentation_process import FermentationConstraintError, FermentationPlan, FermentationResult, MustComposition, NutrientAddition, run_fermentation, validate_must, validate_plan
 from .legal_rules import LegalAwareRegionGrapeRulebook
 from .legal_specs import GrapeConstraint, LegalSpecDecision, LegalSpecRegistry, LegalWineSpec, ReleaseDecision
+from .legal_vineyard_engine import LegalVineyardEngine
 from .origin_factory import ConstrainedOrigin, OriginRequest, WineOriginFactory
 from .priors import SimulationPriors
 from .regional_rules import OriginConstraintError, OriginDecision, RegionGrapeRulebook, RegionRule
 from .schema import *  # noqa: F401,F403 - package intentionally exposes schema types
-from .vineyard_engine import SiteRegistry, VineyardBlock, VineyardEngine, VineyardOutcome
+from .vineyard_engine import SiteRegistry, VineyardBlock, VineyardEngine as BaseVineyardEngine, VineyardOutcome
 from .vintage import load_legacy_vintage_knowledge, vintage_stats
 from .vintage_engine import DailyWeather, VintageDayState, VintageModelParams, VintageOutcome, simulate_vintage
 
+# Public v2 default. The base engine remains available explicitly for low-level
+# tests and non-regulated simulations.
+VineyardEngine = LegalVineyardEngine
+
 __all__ = [
-    "SOURCES", "AlcoholicFermentationParams", "CommercialObservation", "ConstrainedOrigin",
-    "DailyWeather", "FermentationConstraintError", "FermentationPlan", "FermentationResult",
-    "FermentationState", "GrapeConstraint", "LegalAwareRegionGrapeRulebook", "LegalSpecDecision",
-    "LegalSpecRegistry", "LegalWineSpec", "MalolacticParams", "MalolacticState", "MustComposition",
-    "NamedSite", "NutrientAddition", "OriginConstraintError", "OriginDecision", "OriginRequest",
-    "PiwiRecord", "RegionGrapeRulebook", "RegionRule", "ReleaseDecision", "SimulationPriors",
-    "SiteRegistry", "VarietyAreaObservation", "VineyardBlock", "VineyardEngine", "VineyardOutcome",
-    "VintageDayState", "VintageModelParams", "VintageOutcome", "WineKnowledgeCatalog",
-    "WineOriginFactory", "WorldWineKnowledgeCatalog", "initial_state", "load_legacy_vintage_knowledge",
-    "modified_archetype", "normalize_name", "run_alcoholic_fermentation", "run_fermentation",
-    "run_malolactic", "simulate_vintage", "state_at_age", "step_alcoholic_fermentation",
-    "step_malolactic", "validate_must", "validate_plan", "vintage_stats",
+    "SOURCES", "AlcoholicFermentationParams", "BaseVineyardEngine", "CommercialObservation",
+    "ConstrainedOrigin", "DailyWeather", "FermentationConstraintError", "FermentationPlan",
+    "FermentationResult", "FermentationState", "GrapeConstraint", "LegalAwareRegionGrapeRulebook",
+    "LegalSpecDecision", "LegalSpecRegistry", "LegalVineyardEngine", "LegalWineSpec",
+    "MalolacticParams", "MalolacticState", "MustComposition", "NamedSite", "NutrientAddition",
+    "OriginConstraintError", "OriginDecision", "OriginRequest", "PiwiRecord", "RegionGrapeRulebook",
+    "RegionRule", "ReleaseDecision", "SimulationPriors", "SiteRegistry", "VarietyAreaObservation",
+    "VineyardBlock", "VineyardEngine", "VineyardOutcome", "VintageDayState", "VintageModelParams",
+    "VintageOutcome", "WineKnowledgeCatalog", "WineOriginFactory", "WorldWineKnowledgeCatalog",
+    "initial_state", "load_legacy_vintage_knowledge", "modified_archetype", "normalize_name",
+    "run_alcoholic_fermentation", "run_fermentation", "run_malolactic", "simulate_vintage",
+    "state_at_age", "step_alcoholic_fermentation", "step_malolactic", "validate_must",
+    "validate_plan", "vintage_stats",
 ]
