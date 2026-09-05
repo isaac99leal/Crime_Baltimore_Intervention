c91b9ec2baac3abab3206b3385de0b44df1ed44d
isaac99leal <isaac99leal@gmail.com>
2026-09-04 21:43:35 -0400

    Fix vintage ageing generator syntax

--- sommelier-web/src/game/world.ts
+++ sommelier-web/src/game/world.ts
@@ -22,7 +22,7 @@ const MODEL_CURRENT_YEAR = 2026;
 function xmur3(value: string) {
   let h = 1779033703 ^ value.length;
   for (let i = 0; i < value.length; i += 1) {
-    h = Math.imul(h ^ value.charCodeAt(i), 3432918353;
+    h = Math.imul(h ^ value.charCodeAt(i), 3432918353);
     h = h << 13 | h >>> 19;
   }
   return () => {
